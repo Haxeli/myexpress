463ebc5c716c0333d1f2d97b09e3c4567b1cc264
Haxeli <randomfinkiiminki@gmail.com>
2021-02-17 14:53:41 +0200

    mysql suoritettu

---
+++ database.js
@@ -0,0 +1,11 @@
+const sql = require("mysql");
+const connection = sql.createPool(
+    {
+        host:"localhost",
+        user:"netuser",
+        password:"netpass",
+        database:"oamk"
+    }
+);
+
+module.exports = connection;
